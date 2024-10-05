372f06cafe24f0ced54308f8c9444293cb6d187a
amir830047agh@gmail.com <amir830047agh@gmail.com>
2024-10-05 22:05:44 +0330

    api key call and fetch

--- weather-script.js
+++ weather-script.js
@@ -1,11 +1,18 @@
 const searchBtn = document.querySelector(".search-btn");
 const cityInput = document.querySelector(".city-input");
+const API_key = "61e2ffde6b78dcd0f35961896ab8ffdf";
 const getCity = () => {
-  const cityName = cityInput.ariaValueMax.trim();
-  if(!cityName){
-    console.log(cityName);
-    
-  }
+  const cityName = cityInput.value.trim();
+  if (!cityName) return;
+  const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_key}`;
+  fetch(GEOCODING_API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      if (!data.length) return alert(`No coordintes found for ${cityName}`);
+      const [name, lat, lon] = data[0];
+    })
+    .catch(() => {
+      alert("an error ocurred while fetching file");
+    });
 };
-
 searchBtn.addEventListener("click", getCity);
